c6029aa17773044ea080709fae37972bcac2d2fb
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-09 18:18:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,11 @@
 def find_element_index(array,value_to_find)
-
+def find_element_index(array, value)
+  counter=0
+  array=[2,3,4,5]
+  while array[counter]
+    puts array[counter]
+    counter += 1
+  end
 end
 
 def find_max_value(array)
